525700daa60265b13bbf8f118f52e0ec1a288e2e
Achylis21 <francisco.danielivan021@gmail.com>
2023-12-08 23:29:58 +0800

    Added Navbar, tailwind CSS, and React Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Navbar from './Components/navbar'
+
+//Web pages
+import Homepage from './Pages/homepage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Components/navbar.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const Navbar = () => {
+    let [menu, setMenu] = useState(false);
+    return(
+    <div className="navbar bg-slate-800 text-white p-5 w-full sm:w-full lg:w-full lg:w-full mx-auto items-center lg:flex lg:justify-between">
+        <h1 className="text-3xl lg:pl-20">Admin</h1>
+        <div className="text-3x1 absolute right-12 top-8 cursor-pointer lg:hidden">
+            <button onClick={() => setMenu(!menu)}>V</button>
+        </div>
+        <div className="navpages lg:pr-20">
+            <ul className={`navlist list-none z-[-1]
+            absolute w-full bg-slate-800 left-0
+            transition-all duration-700 ease-in-out
+            lg:flex lg:flex-nowrap lg:static lg:z-auto
+            ${menu ? 'top-16':'top-[-490px]'}`}>
+                <li className="cursor-pointer">Create</li>
+                <li className="cursor-pointer">Read All</li>
+                <li className="cursor-pointer">Read Once</li>
+                <li className="cursor-pointer">Delete</li>
+                <div className="logout">
+                    <li className="cursor-pointer">Logout</li>
+                </div>
+            </ul>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar;
