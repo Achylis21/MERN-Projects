5520af05342fc26592c58622c07f0d9819adf727
Achylis21 <francisco.danielivan021@gmail.com>
2023-12-08 23:33:25 +0800

    Added Navbar, tailwind CSS, and React Routes

--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Navbar = () => {
     let [menu, setMenu] = useState(false);
     return(
-    <div className="navbar bg-slate-800 text-white p-5 w-full sm:w-full lg:w-full lg:w-full mx-auto items-center lg:flex lg:justify-between">
+    <div className="navbar bg-slate-800 text-white p-5 w-full sm:w-full md:w-full lg:w-full mx-auto items-center lg:flex lg:justify-between">
         <h1 className="text-3xl lg:pl-20">Admin</h1>
         <div className="text-3x1 absolute right-12 top-8 cursor-pointer lg:hidden">
             <button onClick={() => setMenu(!menu)}>V</button>
